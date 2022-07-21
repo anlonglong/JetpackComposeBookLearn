8722ed33e62492b16c8be2585039f233166b1d42
anlonglong <846506410@qq.com>
2022-07-21 21:26:46 +0800

    Modify simple use

--- app/src/main/java/com/longlong/an/jetpackcomposebooklearn/chapter4/BoxActivity.kt
+++ app/src/main/java/com/longlong/an/jetpackcomposebooklearn/chapter4/BoxActivity.kt
@@ -2,12 +2,18 @@ package com.longlong.an.jetpackcomposebooklearn.chapter4
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
 
 class BoxActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    @Composable
+    fun BoxTest(){
+
     }
 
 }
